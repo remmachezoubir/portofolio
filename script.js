a82137b06bc4845bd12c013d663bb14d4e3e764f
zoubir <remmachezoubir7@gmail.com>
2022-10-03 20:01:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var mybuttons = document.getElementsByTagName("button");
+
+var myBs = document.getElementsByTagName("h2");
+
+var myPs = document.getElementsByTagName("li");
+var main = document.querySelector("main");
+function hide0(){
+    myBs[0].style.visibility="visible";
+    myPs[0].style.visibility="visible";
+    
+
+}
+
+
+function hide1(){
+    myBs[1].style.visibility="visible";
+    myPs[1].style.visibility="visible";
+
+}
+
+function hide2(){
+    myBs[2].style.visibility="visible";
+    myPs[2].style.visibility="visible";
+
+}
+
+function hide3(){
+    myBs[3].style.visibility="visible";
+    myPs[3].style.visibility="visible";
+
+}
+
+
+
+
